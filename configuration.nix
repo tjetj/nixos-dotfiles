30306dc3ff1ee05b4dd5d72232ddff681426f035
tjetj <168307178+tjetj@users.noreply.github.com>
2026-07-14 15:58:43 -0400

    Add support for bluetooth headphones

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@
 
   programs.hyprland = {
     enable = true;
-    withUWSM = true;
+#    withUWSM = true;
     xwayland.enable = true;
   };
 
@@ -37,15 +37,61 @@
     ];
   };
 
+  security.rtkit.enable = true;
+  
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    wireplumber.enable = true;
+  };
+  
+  hardware.pulseaudio.enable = false;
+
   environment.systemPackages = with pkgs; [
     neovim
     wget
     nerd-fonts.jetbrains-mono
     krita
     git
+    alsa-utils
     home-manager
+
+    # encryption
+    gnupg
   ];
 
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
+
+
+  networking.firewall.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "26.05";
 }
