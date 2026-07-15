1080593822e3f3a70398ebd351b2f55526e8c57d
tjetj <168307178+tjetj@users.noreply.github.com>
2026-07-14 20:56:49 -0400

    Added keybindings + gruvbox

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,29 +1,47 @@
-{ config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 
 {
-  # Install Neovim and dependencies
   home.packages = with pkgs; [
-    # Tools required for Telescope
+    # Telescope dependencies
     ripgrep
     fd
     fzf
 
-    # Language Servers
+    # Language servers
     lua-language-server
-    nil # nix language server
-    nixpkgs-fmt # nix formatter
-    #nodejs
+    nil
+    nixpkgs-fmt
   ];
 
   programs.neovim = {
     enable = true;
+
     viAlias = true;
     vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-        telescope-nvim
-        nvim-treesitter
-        nvim-lspconfig
-    ];
-  };
+    vimdiffAlias = true;
 
+    plugins = with pkgs.vimPlugins; [
+      gruvbox-nvim
+      telescope-nvim
+      nvim-treesitter
+      nvim-lspconfig
+    ];
+
+    extraLuaConfig = ''
+
+      -- Theme
+      vim.o.termguicolors = true
+      vim.cmd.colorscheme("gruvbox")
+
+      -- Keybindings
+      local builtin = require("telescope.builtin")
+      vim.g.mapleader = " "
+      vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+      vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+    '';
+  };
 }
